1d8750c2bba57d92b7b512b5d9dafdd2c7442e50
chunky152 <alvinmuwanguzi1@gmail.com>
2025-10-29 12:41:31 +0300

    created the test folder

--- tests/database.test.js
+++ tests/database.test.js
@@ -1,20 +1,21 @@
-import { config } from 'dotenv';
-import { connect, connection, disconnect } from 'mongoose';
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-config();
+
+dotenv.config();
 
 describe('MongoDB connection', () => {
 	beforeAll(async () => {
 		const uri = process.env.MONGO_URI;
 		if (!uri) throw new Error('MONGO_URI not set in .env');
-		await connect(uri);
+		await mongoose.connect(uri);
 	}, 20000);
 
 	it('should connect to MongoDB and have readyState = 1', () => {
-		expect(connection.readyState).toBe(1);
+		expect(mongoose.connection.readyState).toBe(1);
 	});
 
 	afterAll(async () => {
-		await disconnect();
+		await mongoose.disconnect();
 	});
 });
